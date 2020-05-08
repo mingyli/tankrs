099161c3818641e87f8e394bbf8f46b836c2b65b
ming li <mingyli34@gmail.com>
2020-05-07 20:39:25 -0700

    Add base schema for world state

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,24 @@
+#[allow(dead_code, unused_imports, clippy::redundant_field_names)]
+#[path = "../../schema/world_generated.rs"]
+mod world_generated;
+pub use world_generated::tankrs::{get_root_as_world, World, WorldArgs};
+
 fn main() {
-    println!("TANKRS");
+    let mut builder = flatbuffers::FlatBufferBuilder::new_with_capacity(1024);
+    {
+        let world = World::create(
+            &mut builder,
+            &WorldArgs {
+                width: 30,
+                height: 20,
+            },
+        );
+        builder.finish(world, None);
+    }
+
+    let buffer = builder.finished_data();
+    let world = get_root_as_world(buffer);
+    let width = world.width();
+    let height = world.height();
+    println!("My world has width {} and height {}.", width, height);
 }
