4cbd98de53622465bd023369be3498fb8b737dba
ming li <mingyli34@gmail.com>
2020-05-07 21:23:04 -0700

    Use protobufs instead

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,24 +1,15 @@
-#[allow(dead_code, unused_imports, clippy::redundant_field_names)]
-#[path = "../../schema/world_generated.rs"]
-mod world_generated;
-pub use world_generated::tankrs::{get_root_as_world, World, WorldArgs};
+mod protos;
+use protos::world::World;
 
 fn main() {
-    let mut builder = flatbuffers::FlatBufferBuilder::new_with_capacity(1024);
-    {
-        let world = World::create(
-            &mut builder,
-            &WorldArgs {
-                width: 30,
-                height: 20,
-            },
-        );
-        builder.finish(world, None);
-    }
-
-    let buffer = builder.finished_data();
-    let world = get_root_as_world(buffer);
-    let width = world.width();
-    let height = world.height();
-    println!("My world has width {} and height {}.", width, height);
+    let world = World {
+        width: 30,
+        height: 20,
+        ..Default::default()
+    };
+    println!(
+        "My world has width {} and height {}.",
+        world.get_width(),
+        world.get_height()
+    );
 }
