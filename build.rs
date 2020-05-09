db7f1f04cdb9c51e57e2d16440cfebef1a9f1541
ming li <mingyli34@gmail.com>
2020-05-08 20:10:09 -0700

    Add flatbuffers for base world state (#7)

---
+++ build.rs
@@ -0,0 +1,11 @@
+use std::path::Path;
+
+fn main() {
+    println!("cargo:rerun-if-changed=schema/");
+    flatc_rust::run(flatc_rust::Args {
+        inputs: &[Path::new("schema/world.fbs")],
+        out_dir: Path::new("schema/rust/"),
+        ..flatc_rust::Args::default()
+    })
+    .expect("flatc failed to compile the flatbuffers.");
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,35 @@
+#[allow(dead_code, unused_imports, clippy::all)]
+#[rustfmt::skip]
+#[path = "../../schema/rust/world_generated.rs"]
+mod world_generated;
+use world_generated::tankrs::{get_root_as_world, World, WorldArgs};
+
+impl World<'_> {
+    // Demo custom methods.
+    fn foo(&self) -> u16 {
+        self.width() * self.height()
+    }
+}
+
 fn main() {
-    println!("TANKRS");
+    let mut builder = flatbuffers::FlatBufferBuilder::new_with_capacity(1024);
+    {
+        let world = World::create(
+            &mut builder,
+            &WorldArgs {
+                width: 40,
+                height: 30,
+            },
+        );
+        builder.finish(world, None);
+    }
+
+    let buffer = builder.finished_data();
+    let world = get_root_as_world(buffer);
+    println!(
+        "My world has width {} and height {}.",
+        world.width(),
+        world.height()
+    );
+    println!("foo: {}", world.foo());
 }
