367b5d41df129815a430cfc6178ef725d6ba5b63
ming li <mingyli34@gmail.com>
2020-05-09 12:38:32 -0700

    Set up async foundation on server side (#12)
    * Set up async foundation on server side
    
    * Print received address

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,28 +1,31 @@
-use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
-use std::thread::spawn;
+use std::time;
 
+use async_std::net::{TcpListener, TcpStream};
+use async_std::task;
+use futures::prelude::*;
 use tungstenite::Message;
 
 use schema::world_generated::{World, WorldArgs};
 
-fn handle_client(stream: TcpStream, _my: &Arc<Vec<u8>>) -> tungstenite::Result<()> {
-    use std::{thread, time};
-    let mut socket = tungstenite::accept(stream).unwrap();
+async fn handle_client(stream: TcpStream, my: Arc<Vec<u8>>) -> anyhow::Result<()> {
+    let mut ws_stream = async_tungstenite::accept_async(stream).await?;
     println!("Running test");
     let x_end: f64 = 10.0;
     let tick_rate = time::Duration::from_millis(1000 / 60);
     let ticks = 3 * 60;
+
     loop {
+        ws_stream.send(Message::Binary(my.to_vec())).await?;
         for i in 0..ticks {
             let pos: f64 = (x_end / f64::from(ticks)) * f64::from(i);
-            socket.write_message(Message::Text(pos.to_string()))?;
-            thread::sleep(tick_rate);
+            ws_stream.send(Message::Text(pos.to_string())).await?;
+            task::sleep(tick_rate).await;
         }
     }
 }
 
-fn main() {
+async fn run() -> anyhow::Result<()> {
     let mut builder = flatbuffers::FlatBufferBuilder::new_with_capacity(1024);
     let world = World::create(
         &mut builder,
@@ -33,25 +36,18 @@ fn main() {
     );
     builder.finish(world, None);
     let buffer = builder.finished_data().to_vec();
-
     let world_arc = Arc::new(buffer);
 
-    let server = TcpListener::bind("127.0.0.1:9001").unwrap();
+    let server = TcpListener::bind("127.0.0.1:9001").await?;
     println!("Starting server");
-    for stream in server.incoming() {
+    while let Ok((stream, address)) = server.accept().await {
+        println!("Received on address {}", address);
         let w = world_arc.clone();
-        spawn(move || match stream {
-            Ok(stream) => {
-                if let Err(err) = handle_client(stream, &w) {
-                    match err {
-                        tungstenite::Error::ConnectionClosed
-                        | tungstenite::Error::Protocol(_)
-                        | tungstenite::Error::Utf8 => (),
-                        e => eprintln!("test: {}", e),
-                    }
-                }
-            }
-            Err(e) => eprintln!("Error accepting stream: {}", e),
-        });
+        task::spawn(handle_client(stream, w));
     }
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    task::block_on(run())
 }
