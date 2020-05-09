c2bc09e9078a1251200042f58d61725b7517e2be
mluogh <mluogh@gmail.com>
2020-05-08 20:45:30 -0700

    Slightly better way to get rid of rustfmt/clippy check?

---
+++ schema/schema.rs
@@ -0,0 +1,4 @@
+#[allow(dead_code, unused_imports, clippy::all)]
+#[rustfmt::skip]
+#[path = "./rust/world_generated.rs"]
+pub mod world_generated;
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,6 @@
-#[allow(dead_code, unused_imports, clippy::all)]
-#[rustfmt::skip]
-#[path = "../../schema/rust/world_generated.rs"]
-mod world_generated;
-use world_generated::tankrs::{get_root_as_world, World, WorldArgs};
+#[path = "../../schema/schema.rs"]
+mod schema;
+use schema::world_generated::{get_root_as_world, World, WorldArgs};
 
 impl World<'_> {
     // Demo custom methods.
