6cfb16e94be93c550afa6c8b8926c5ff56b3d863
ming li <mingyli34@gmail.com>
2020-05-09 01:27:10 -0700

    Write simple demonstration of websocket client and server (#9)
    * Write simple demonstration of websocket client and server
    
    * -

---
+++ server/src/client.rs
@@ -0,0 +1,38 @@
+use tungstenite::{connect, Message};
+use url::Url;
+
+use schema::world_generated::get_root_as_world;
+
+fn main() {
+    let (mut socket, response) =
+        connect(Url::parse("ws://localhost:9001/socket").unwrap()).expect("Can't connect");
+
+    println!("Connected to the server");
+    println!("Response HTTP code: {}", response.status());
+    println!("Response contains the following headers:");
+    for (ref header, _value) in response.headers() {
+        println!("* {}", header);
+    }
+
+    socket
+        .write_message(Message::Text("Hello WebSocket".into()))
+        .unwrap();
+    loop {
+        let msg = socket.read_message().expect("Error reading message");
+        println!("Received: {:?}", msg);
+        match msg {
+            Message::Binary(buffer) => {
+                let world = get_root_as_world(&buffer);
+                println!(
+                    "My world has width {} and height {}.",
+                    world.width(),
+                    world.height()
+                );
+            }
+            Message::Text(text) => {
+                println!("Received text: {}", text);
+            }
+            _ => {}
+        }
+    }
+}
