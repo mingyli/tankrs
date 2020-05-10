c232122703d331059c8d56b23480324c39e70919
mluogh <mluogh@gmail.com>
2020-05-10 00:07:59 -0700

    Add typescript flatbuffers

--- schema/rust/build.rs
+++ schema/rust/build.rs
@@ -3,9 +3,18 @@ use std::path::Path;
 fn main() {
     println!("cargo:rerun-if-changed=schema");
     flatc_rust::run(flatc_rust::Args {
+        lang: "rust",
         inputs: &[Path::new("../world.fbs")],
         out_dir: Path::new("src"),
         ..flatc_rust::Args::default()
     })
-    .expect("flatc failed to compile the flatbuffers.");
+    .expect("flatc failed to compile the flatbuffers for rust");
+
+    flatc_rust::run(flatc_rust::Args {
+        lang: "ts",
+        inputs: &[Path::new("../world.fbs")],
+        out_dir: Path::new("../typescript/"),
+        ..Default::default()
+    })
+    .expect("flatc failed to compile the flatbuffers for typescript");
 }
