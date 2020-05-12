cd5f9d374377ae2ee5a011d30c82fc1ec2b5df98
Michael Luo <8098155+mluogh@users.noreply.github.com>
2020-05-11 23:57:56 -0700

    Update schema (#22)
    * Update schema
    
    * update comment
    
    * put vec2 in own schema file
    
    * remove worldstate as root
    
    * Add comments to table definitions

--- schema/rust/build.rs
+++ schema/rust/build.rs
@@ -3,7 +3,12 @@ use std::path::Path;
 fn main() {
     println!("cargo:rerun-if-changed=schema");
     flatc_rust::run(flatc_rust::Args {
-        inputs: &[Path::new("../world.fbs")],
+        inputs: &[
+            Path::new("../world.fbs"),
+            Path::new("../messages.fbs"),
+            Path::new("../actions.fbs"),
+            Path::new("../math.fbs"),
+        ],
         out_dir: Path::new("src"),
         ..flatc_rust::Args::default()
     })
