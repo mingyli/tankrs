48140a050f605a847769ba475d32737fef91b9e1
ming li <mingyli34@gmail.com>
2020-05-11 20:13:44 -0700

    Add block

--- server/src/world.rs
+++ server/src/world.rs
@@ -1,25 +1,37 @@
 use super::serialize::Serializable;
 use schema::world_generated;
-
+pub struct Block {
+    x: f32,
+}
 pub struct World {
     width: u16,
     height: u16,
+    block: Block,
 }
 
 impl World {
     pub fn new(width: u16, height: u16) -> World {
-        World { width, height }
+        World {
+            width,
+            height,
+            block: Block { x: 0.0 },
+        }
     }
 }
 
 impl Serializable for World {
     fn serialize(&self) -> Vec<u8> {
         let mut builder = flatbuffers::FlatBufferBuilder::new_with_capacity(1024);
+        let block = world_generated::Block::create(
+            &mut builder,
+            &world_generated::BlockArgs { x: self.block.x },
+        );
         let world = world_generated::World::create(
             &mut builder,
             &world_generated::WorldArgs {
                 width: self.width,
                 height: self.height,
+                block: Some(block),
             },
         );
         builder.finish(world, None);
